c2ab09837be71604a5cefbf6b68ad8d01d78497c
Joe Maples <joe@maples.dev>
2021-01-17 17:50:36 +0530

    Settings: Add Bluetooth panel
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- src/com/android/settings/panel/PanelFeatureProviderImpl.java
+++ src/com/android/settings/panel/PanelFeatureProviderImpl.java
@@ -49,6 +49,8 @@ public class PanelFeatureProviderImpl implements PanelFeatureProvider {
                 return VolumePanel.create(context);
             case ACTION_MEDIA_OUTPUT_GROUP:
                 return MediaOutputGroupPanel.create(context, mediaPackageName);
+            case Settings.Panel.ACTION_BLUETOOTH:
+                return BluetoothPanel.create(context);
         }
 
         throw new IllegalStateException("No matching panel for: "  + panelType);
